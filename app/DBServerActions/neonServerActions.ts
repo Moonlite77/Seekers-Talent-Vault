65c119c71cca4a0719a029674605e155c35b6b18
Moonlite77 <168150327+Moonlite77@users.noreply.github.com>
2025-10-30 16:34:01 +0000

    wrote basic server actions for database interactions

---
+++ app/DBServerActions/neonServerActions.ts
@@ -0,0 +1,40 @@
+'use server'
+
+import { neon } from "@neondatabase/serverless"
+
+
+const sql = neon(process.env.DATABASE_URL!)
+
+export async function getBasicData(clerkID: string){
+
+    const result = await sql`
+    SELECT * FROM clerks WHERE clerk_id = ${clerkID};
+    `
+
+    return(
+        result
+    )
+}
+
+export async function createUser(clerkID: string){
+
+    const result = await sql`
+    INSERT INTO clerks (clerk_id)
+    VALUES (${clerkID})
+    `
+
+    return(
+        result
+    )
+}
+
+export async function updateArch(clerkID: string, archetype: string){
+
+    const result = await sql`
+    UPDATE clerks SET archetype = ${archetype} WHERE clerk_id = ${clerkID}
+    `
+
+    return(
+        result
+    )
+}
--- app/chooseArch/page.tsx
+++ app/chooseArch/page.tsx
@@ -1,15 +1,38 @@
-import MainCard from "../custom-components/main-card"
+'use client'
 
-//to-do create a clark action -> create user in a database whenever a clark user is created
-//first checks database for "archetype"
-//then checks localstorage for Archetype.
-//previous data ? popupAskingContinueOrDeletePrevData : doNothing
+import { getBasicData, createUser, updateArch } from "@/app/DBServerActions/neonServerActions"
+import { useRouter } from "next/navigation"
+import { useUser } from "@clerk/nextjs"
+//pull basic information for the clerk_id in question
 
-//If user clicks seeker -> Creates all the localstrorage strings and writes 'seeker' for archetype
-//If user clicks champion -> Creates all the localstrorage strings and writes 'champion' for archetype
+// if clerk_id is there and they have an avatar_url -> send them to town
+// if there's no user -> create the user, continue choosing
+
+//when user clicks on either champion or seeker, update the DB and continue to onboarding
+
+
 
 export default function ChooseArch(){
+
+    const { user } = useUser();
+    const router = useRouter();
+
+    const clerkUser = user?.id
+
+    function seekerClick() {
+        console.log('seeker!');
+        router.push('/seeker-onboard')
+    }
+
+        function champClick() {
+        console.log('Champion!');
+        router.push('/talent-onboard')
+    }
     return(
-        <div className="text-white z-2">Choose Your Archetype</div>
+        <>
+            <div className="text-white z-2">Choose Your Archetype</div>
+            <button className="text-white z-2" onClick={seekerClick}> Seeker </button>
+            <button className="text-white z-2" onClick={champClick}> Champion </button>
+        </>
     )
 }
