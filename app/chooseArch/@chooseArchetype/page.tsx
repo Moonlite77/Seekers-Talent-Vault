183c881707fc45a5d1f560f3f90f292f74739403
Moonlite77 <168150327+Moonlite77@users.noreply.github.com>
2025-11-03 15:21:33 +0000

    added cards to the chooseArchetype route

--- app/chooseArch/@chooseArchetype/page.tsx
+++ app/chooseArch/@chooseArchetype/page.tsx
@@ -1,11 +1,21 @@
 import { SeekerButton, ChampButton } from "../archButtons"
+import MainCard from "@/app/custom-components/main-card"
 
 export default async function chooseArchPage(){
     return(
-                <div>
-                    <div className="text-white z-3">Choose Your Archetype</div>
-                    <SeekerButton />
-                    <ChampButton />
+                <div className="grid grid-cols-2 z-3">
+                    <div className="col-span-2 text-white">Choose Your Archetype</div>
+                    <MainCard
+                        imageSrc="https://imagedelivery.net/_3BvaaU0nebybABLZIjMPA/9a03db4c-fbeb-461c-e0ae-6090a2760000/public"
+                        cardLink="/seeker-onboard" 
+                        cardText="For the schrewd recruiter or anyone who seeks champions looking for opportunities."
+                            />
+                    <MainCard
+                        imageSrc="https://imagedelivery.net/_3BvaaU0nebybABLZIjMPA/b4869a9d-6cd3-496e-416e-2d3ae143c200/public"
+                        cardLink="/talent-onboard" 
+                        cardText="For the talented individuals who seek opportunities."
+                            />
+
                 </div>
     )
 }
--- app/pre-boarding/page.tsx
+++ app/pre-boarding/page.tsx
@@ -6,6 +6,7 @@ import PBComponent from "../dbClientComponents/pre-boardingComponent";
 import { Suspense } from 'react'
 
 
+//TO DO - Get rid of this route. Keeping for now in case I need a refresher on react's 'use' hook and passing data to and from components.
 
 export default async function PreboardingComponent(){
     const user = await currentUser()
