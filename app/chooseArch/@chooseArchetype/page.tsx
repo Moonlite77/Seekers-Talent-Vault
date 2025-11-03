4c16db7260dff0d51732dad42a783bc7817ec21d
Moonlite77 <168150327+Moonlite77@users.noreply.github.com>
2025-11-03 15:33:14 +0000

    created the link-card component and centered some divs

--- app/chooseArch/@chooseArchetype/page.tsx
+++ app/chooseArch/@chooseArchetype/page.tsx
@@ -1,16 +1,18 @@
 import { SeekerButton, ChampButton } from "../archButtons"
 import MainCard from "@/app/custom-components/main-card"
+import LinkCard from "@/app/custom-components/link-card"
 
 export default async function chooseArchPage(){
     return(
-                <div className="grid grid-cols-2 z-3">
-                    <div className="col-span-2 text-white">Choose Your Archetype</div>
-                    <MainCard
+                <div className="grid grid-cols-3 z-3">
+                    
+                    <LinkCard
                         imageSrc="https://imagedelivery.net/_3BvaaU0nebybABLZIjMPA/9a03db4c-fbeb-461c-e0ae-6090a2760000/public"
                         cardLink="/seeker-onboard" 
-                        cardText="For the schrewd recruiter or anyone who seeks champions looking for opportunities."
+                        cardText="For the schrewd recruiter or talent seeker."
                             />
-                    <MainCard
+                    <div className="justify-items-center items-center text-white">Choose Your Archetype</div>
+                    <LinkCard
                         imageSrc="https://imagedelivery.net/_3BvaaU0nebybABLZIjMPA/b4869a9d-6cd3-496e-416e-2d3ae143c200/public"
                         cardLink="/talent-onboard" 
                         cardText="For the talented individuals who seek opportunities."
